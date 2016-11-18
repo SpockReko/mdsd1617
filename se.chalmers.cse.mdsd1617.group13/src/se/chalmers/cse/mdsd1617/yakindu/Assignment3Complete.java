d0c5ada8535fe55a0ca30dc054d91900fe800e57
SpockReko <stefan.fritzon@gmail.com>
2016-11-18 11:39:03 +0100

    98 procent ready I think

--- se.chalmers.cse.mdsd1617.group13/src/se/chalmers/cse/mdsd1617/yakindu/Assignment3Complete.java
+++ se.chalmers.cse.mdsd1617.group13/src/se/chalmers/cse/mdsd1617/yakindu/Assignment3Complete.java
@@ -4,7 +4,7 @@ public class Assignment3Complete {
 	private long currentBookingId = 0;
 	private long currentReservationNumber = 0;
 
-	private final int MAX_ROOMS = 2;
+	private final int MAX_ROOMS = 3;
 	
 	private Room[] roomStatus = new Room[MAX_ROOMS];
 	private enum Room{FREE,ASSIGNED,OCCUPIDE};
@@ -27,7 +27,7 @@ public class Assignment3Complete {
 			return false;
 		} else {
 			++currentReservationNumber;
-			return true;
+			return freeRoom((long) currentReservationNumber);
 		}
 	}
 	
@@ -38,33 +38,45 @@ public class Assignment3Complete {
 		return bookingStatus[(int)bookingId] == Booking.CONFIRMED;
 	}
 	
-	/*
+	
 	public boolean checkInBooking(long bookingId){
-		return false;
+		bookingStatus[(int)bookingId] = Booking.CHECKEDIN;
+		return bookingStatus[(int)bookingId] == Booking.CHECKEDIN;
+	
 	}
 	
 	public boolean initiateCheckout(long bookingId){
-		return false;
+		bookingStatus[(int)bookingId] = Booking.CHECKEDOUT;
+		return bookingStatus[(int)bookingId] == Booking.CHECKEDOUT;
 	}
 	
 	public boolean payDurningCheckout(long bookingId){
-		return false;
+		bookingStatus[(int)bookingId] = Booking.PAYED;
+		return bookingStatus[(int)bookingId] == Booking.PAYED;
 	}
 	
-	*/
+	
 	/*************************/
 	/**reservation interface**/
 	/*************************/
-	
+	// 2 methods that returns the index in the arrays.
 	public long getBookingId(){
 		return currentBookingId;
 	}
 	
+	public long getRoomId(){
+		return currentReservationNumber;
+	}
+	// 2 methods that returns the id's.
 	public long getCurrentReservationNumber(){
 		return currentReservationNumber;
 	}
 	
+	public long getCurrentBookingNumber(){
+		return currentBookingId;
+	}
 	
+	// 3 methods that change the room state
 	public boolean assignRoom(long roomIndex){
 		int index = (int) roomIndex;
 		roomStatus[index] = Room.ASSIGNED;
@@ -83,13 +95,53 @@ public class Assignment3Complete {
 		return roomStatus[index] == Room.FREE;	
 	}
 	
+	public boolean bookingConfirmed(long bookingId){
+		int index = (int) bookingId;
+		return bookingStatus[index] == Booking.CONFIRMED;
+	}
+	
+	public boolean bookingCheckedin(long bookingId){
+		int index = (int) bookingId;
+		return bookingStatus[index] == Booking.CHECKEDIN;
+	}
+	
+	public boolean bookingCheckedout(long bookingId){
+		int index = (int) bookingId;
+		return bookingStatus[index] == Booking.CHECKEDOUT;
+	}
+	
+	public boolean checkFreeRoom(long roomId){ 
+		int index = (int) roomId;
+		return roomStatus[index] == Room.FREE;	
+	}
+	public boolean checkAssignedRoom(long roomId){
+		int index = (int) roomId;
+		return roomStatus[index] == Room.ASSIGNED;	
+	
+	}
+	public boolean checkOccupideRoom(long roomId){
+		int index = (int) roomId;
+		return roomStatus[index] == Room.OCCUPIDE;	
+	}
+	
+	
 	/***********************/
 	/**** room interface ***/
 	/***********************/
 	
-	public boolean roomAssigned(long roomIndex){return false;} 
-	public boolean roomOccupide(long roomIndex){return false;}
-	public boolean roomFree(long roomIndex){return false;}
+	public boolean roomAssigned(long roomIndex){	
+		int index = (int) roomIndex;
+		return roomStatus[index] == Room.ASSIGNED;	
+	} 
+	public boolean roomOccupide(long roomIndex){	
+		int index = (int) roomIndex;
+		return roomStatus[index] == Room.OCCUPIDE;	
+	}
+	public boolean roomFree(long roomIndex){
+		int index = (int) roomIndex;
+		return roomStatus[index] == Room.FREE;	
+	}
+	
 	
 	
 }
