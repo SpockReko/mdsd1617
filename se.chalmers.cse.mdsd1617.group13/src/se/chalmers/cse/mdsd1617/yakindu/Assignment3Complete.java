2c7410d8b0767ec90bab1ee3d793ae69b3be591b
vchristoffersson <vchristoffersson@gmail.com>
2016-11-20 18:01:25 +0100

    assignment3

--- se.chalmers.cse.mdsd1617.group13/src/se/chalmers/cse/mdsd1617/yakindu/Assignment3Complete.java
+++ se.chalmers.cse.mdsd1617.group13/src/se/chalmers/cse/mdsd1617/yakindu/Assignment3Complete.java
@@ -3,9 +3,17 @@ package se.chalmers.cse.mdsd1617.yakindu;
 public class Assignment3Complete {
 	private long currentBookingId = 0;
 	private long currentReservationNumber = 0;
-
+	
 	private final int MAX_ROOMS = 5;
+	private boolean[] freeRooms = new boolean[MAX_ROOMS];
 
+	
+	public Assignment3Complete(){
+		for(int i = 0; i < freeRooms.length; i++){
+			freeRooms[i] = true;
+		}
+	}
+	
 	public long initiateBooking() {
 		return ++currentBookingId;
 	}
@@ -19,5 +27,29 @@ public class Assignment3Complete {
 			++currentReservationNumber;
 			return true;
 		}
+	}
+	
+	public long getCurrentReservationNumber(){
+		return currentReservationNumber;
+	}
+	
+	public long checkFreeRooms(){
+		long i = 0;
+		for(; i < freeRooms.length; i++){
+			if(freeRooms[(int) i]){
+				//freeRooms[(int) i] = false;
+				return i;
+			}
+		} 
+		return -1;
+	}
+	
+	public void occupyRoom(long roomId){
+		freeRooms[(int) roomId] = false;
+	}
+	
+	public long checkOut(long roomId){
+		freeRooms[(int)roomId] = true;
+		return roomId;
 	}
 }
