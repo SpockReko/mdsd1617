1dd93ebbc877d493aecae246dcf99dc5bcd9f7c1
Martin Tran <martin_t_39@hotmail.com>
2016-11-21 09:31:12 +0100

    Add comments in the java file

--- se.chalmers.cse.mdsd1617.group13/src/se/chalmers/cse/mdsd1617/yakindu/Assignment3Complete.java
+++ se.chalmers.cse.mdsd1617.group13/src/se/chalmers/cse/mdsd1617/yakindu/Assignment3Complete.java
@@ -14,10 +14,12 @@ public class Assignment3Complete {
 		return ++currentBookingId;
 	}
 	
+	// Add a reservation to the booking.
+	// Returns false if the reservations exceeds the current max rooms
 	public boolean addRoomToBooking(long bookingId) {
 		if (bookingId < 1 || bookingId > currentBookingId) {
 			return false;
-		} else if (currentReservationNumber >= MAX_ROOMS) {
+		} else if (currentReservationNumber >= (MAX_ROOMS-1)) {
 			return false;
 		} else {
 			currentSelectedBookingId = bookingId;
@@ -26,64 +28,79 @@ public class Assignment3Complete {
 		}
 	}
 	
+	// Marks what the current reservation is
+	// Helps to connect a reservation to a room
 	public boolean addReservationToRoom(long reservationId) {
 		currentSelectedReservationId = reservationId;
 		++currentRoomId;
 		return true;
 	}
 	
+	// Confirms the booking
+	// TO-DO: return false if there is no added room to the booking
 	public boolean confirmBooking(long bookingId) {
 		currentSelectedBookingId = bookingId;
 		return true;
 	}
 	
+	// Checks in the booking
 	public boolean checkInBooking(long bookingId) {
 		currentSelectedBookingId = bookingId;
 		return true;
 	}
 	
+	// Initiate a checkout of the room (booking)
 	public boolean initiateCheckout(long bookingId) {
 		currentSelectedBookingId = bookingId;
 		return true;
 	}
 	
+	// The finish state that end the booking with payment 
 	public boolean payDuringCheckout(long bookingId) {
 		currentSelectedBookingId = bookingId;
 		return true;
 	}
 	
+	// Assign a room to its reservation
 	public boolean assignRoom(long reservationId) {
 		currentSelectedReservationId = reservationId;
 		return true;
 	}
 	
+	// Mark the room as occupied. Happens when you checks in the booking
 	public boolean occupiedRoom(long reservationId) {
 		currentSelectedReservationId = reservationId;
 		return true;
 	}
 	
+	// Mark the room as free. Happens when you check out the booking
 	public boolean freeRoom(long reservationId) {
 		currentSelectedReservationId = reservationId;
 		return true;
 	}
 	
+	// Mark the room as free. Happens when pay during checkout
 	public boolean endReservationRoom(long reservationId) {
 		currentSelectedReservationId = reservationId;
 		return true;
 	}
 	
+	// Get the current selected bookingId
 	public long getCurrentSelectedBookingId() {
 		return currentSelectedBookingId;
 	}
 	
+	// Get the current selected reservationId
 	public long currentSelectedReservationId() {
 		return currentSelectedReservationId;
 	}
 	
+	// Get the up-most reservationId
 	public long getCurrentReservationNumber() {
 		return currentReservationNumber;
 	}
 	
+	// Get the up-most roomId
 	public long getCurrentRoomId() {
 		return currentRoomId;
 	}
