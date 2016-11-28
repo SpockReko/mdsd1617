a0a543d50c829b60f3a3c12f06bf54ca00a10bd6
elmhaidara <elmhaidara@gmail.com>
2016-11-28 10:54:15 +0100

    Removed unused imports from Booking.java

--- se.chalmers.cse.mdsd1617.group13/src/se/chalmers/cse/mdsd1617/yakindu/Booking.java
+++ se.chalmers.cse.mdsd1617.group13/src/se/chalmers/cse/mdsd1617/yakindu/Booking.java
@@ -1,10 +1,7 @@
 package se.chalmers.cse.mdsd1617.yakindu;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.ListIterator;
-import java.util.Queue;
 
 public class Booking {
 
