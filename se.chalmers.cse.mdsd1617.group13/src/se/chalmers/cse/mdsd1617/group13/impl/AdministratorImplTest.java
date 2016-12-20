9fe3788003ea03b963e43c4a44369d1310dd9e42
vchristoffersson <vchristoffersson@gmail.com>
2016-12-20 12:04:57 +0100

    started testing admin uc

---
+++ se.chalmers.cse.mdsd1617.group13/src/se/chalmers/cse/mdsd1617/group13/impl/AdministratorImplTest.java
@@ -0,0 +1,21 @@
+package se.chalmers.cse.mdsd1617.group13.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+
+public class AdministratorImplTest {
+
+	@BeforeClass
+	
+	
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
